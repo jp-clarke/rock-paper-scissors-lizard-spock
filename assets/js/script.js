bdff3f6ecc3e9d5f774da3a26950e7850d1ee235
John Clarke <jp_clarke@outlook.com>
2023-02-04 13:48:43 +0000

    Add playRock functon

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,19 +21,33 @@ function playGame(playerPicks) {
     let computerPicks = Math.floor(Math.random() * 5);
     
     if (playerPicks === "player-rock") {
-        console.log(playerPicks);
-        console.log(computerPicks);
+        playRock(computerPicks);
     } else if (playerPicks === "player-paper") {
         console.log(playerPicks);
         console.log(computerPicks);
-    } else if (playerPicks === "player-paper") {
+    } else if (playerPicks === "player-scissors") {
         console.log(playerPicks);
         console.log(computerPicks);
-    } else if (playerPicks === "player-paper") {
+    } else if (playerPicks === "player-lizard") {
         console.log(playerPicks);
         console.log(computerPicks);
     } else {
         console.log(playerPicks);
         console.log(computerPicks);
+    }
+}
+
+// Player picks rock
+function playRock(computerPicks) {
+    if (computerPicks === 0) {
+        alert("DRAW\nYou chose Rock\nCOM chose Rock");
+    } else if (computerPicks === 1) {
+        alert("LOSE\nYou chose Rock\nCOM chose Paper");
+    } else if (computerPicks === 2) {
+        alert("WIN\nYou chose Rock\nCOM chose Scissors");
+    } else if (computerPicks === 3) {
+        alert("WIN\nYou chose Rock\nCOM chose Lizard");
+    } else {
+        alert("LOSE\nYou chose Rock\nCOM chose Spock");
     }
 }
