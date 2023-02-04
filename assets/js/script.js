cc56f7b01860257b4de32905e04db70c6f68ab20
John Clarke <jp_clarke@outlook.com>
2023-02-04 14:22:25 +0000

    Add select array, make pick variables global

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// Declare global variables
+let playerPicks;
+let computerPicks;
+const select = ["rock", "paper", "scissors", "lizard", "spock"]
+
 // Wait for DOM to load and add event listeners for buttons
 // Code from Love Maths waklthrough project by Code Institute
 
@@ -9,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "reset-score") {
                 console.log("Reset Scores");
             } else {
-                let playerPicks = this.getAttribute("data-type");
+                playerPicks = this.getAttribute("data-type");
                 playGame(playerPicks);
             }
         });
@@ -18,17 +23,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Player choice and computer choice
 function playGame(playerPicks) {
-    let computerPicks = Math.floor(Math.random() * 5);
+    computerPicks = Math.floor(Math.random() * 5);
     
-    if (playerPicks === "player-rock") {
+    if (playerPicks === "0") {
         playRock(computerPicks);
-    } else if (playerPicks === "player-paper") {
+    } else if (playerPicks === "1") {
         console.log(playerPicks);
         console.log(computerPicks);
-    } else if (playerPicks === "player-scissors") {
+    } else if (playerPicks === "2") {
         console.log(playerPicks);
         console.log(computerPicks);
-    } else if (playerPicks === "player-lizard") {
+    } else if (playerPicks === "3") {
         console.log(playerPicks);
         console.log(computerPicks);
     } else {
@@ -40,14 +45,39 @@ function playGame(playerPicks) {
 // Player picks rock
 function playRock(computerPicks) {
     if (computerPicks === 0) {
+        console.log(playerPicks);
         alert("DRAW\nYou chose Rock\nCOM chose Rock");
     } else if (computerPicks === 1) {
+        console.log(playerPicks);
         alert("LOSE\nYou chose Rock\nCOM chose Paper");
     } else if (computerPicks === 2) {
+        console.log(playerPicks);
         alert("WIN\nYou chose Rock\nCOM chose Scissors");
     } else if (computerPicks === 3) {
+        console.log(playerPicks);
         alert("WIN\nYou chose Rock\nCOM chose Lizard");
     } else {
+        console.log(playerPicks);
         alert("LOSE\nYou chose Rock\nCOM chose Spock");
     }
 }
+
+// Player picks paper
+
+
+// Player picks scissors
+
+
+// Player picks lizard
+
+
+// Player picks spock
+
+
+// Draw function
+
+
+// Win function
+
+
+// Lose function
